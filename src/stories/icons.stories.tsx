3a7d4569c31fb920fdda0a516e12b5002efece50
k-shkrybailo <67151012+k-shkrybailo@users.noreply.github.com>
2021-09-02 16:13:33 +0300

    storybook-svgr-setup (#5)

---
+++ src/stories/icons.stories.tsx
@@ -0,0 +1,44 @@
+import * as Icons from "../components/Icons";
+
+export default {
+  title: "Icons",
+};
+
+const pairs = Object.entries(Icons);
+
+export const Collection = ({
+  size = 14,
+  color = "green",
+}: {
+  size: number;
+  color: string;
+}) => (
+  <div
+    style={{
+      display: "flex",
+      flexFlow: "row wrap",
+      color,
+      fontSize: `${size}px`,
+    }}
+  >
+    {pairs.map(([name, Icon]) => (
+      <div key={name} style={{ margin: "1em", textAlign: "center", color }}>
+        <Icon style={{ height: "3em", fill: color }} />
+        <h4>{name}</h4>
+      </div>
+    ))}
+  </div>
+);
+
+Collection.argTypes = {
+  color: {
+    control: {
+      type: "select",
+      options: ["red", "blue"],
+    },
+  },
+  size: {
+    control: "number",
+    defaultValue: 14,
+  },
+};
