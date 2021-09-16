4816ce82d9e08ca88f0db837b4886ac0c7911fe4
k-shkrybailo <k.shkrybailo@sharksw.com>
2021-09-16 12:42:16 +0300

    input icon styling

--- src/utils/types/themeConfig/index.tsx
+++ src/utils/types/themeConfig/index.tsx
@@ -1,12 +1,19 @@
-import { PaletteOptions } from '@material-ui/core/styles/createPalette';
+import { Theme } from "@material-ui/core";
+import { PaletteOptions } from "@material-ui/core/styles/createPalette";
+import { SpacingOptions } from "@material-ui/core/styles/createSpacing";
 
-import { AnyKeyStringValueObjectType as ColorType } from 'utils/types/common';
+import { AnyKeyStringValueObjectType as ColorType } from "utils/types/common";
 
 export type PaletteDefaultOptionsType = PaletteOptions & {
   primary: ColorType;
   secondary: ColorType;
   status: ColorType;
   input: ColorType;
+};
+
+export type ThemeDefaultOptions = Theme & {
+  palette: PaletteDefaultOptionsType;
+  spacing: SpacingOptions | ((num: number) => number);
 };
 
 export type DeepCloneThemeType = (
