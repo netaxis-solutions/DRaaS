90ecc333cfcb0d6a6a8c7da7082a7dfc542fd993
k-shkrybailo <67151012+k-shkrybailo@users.noreply.github.com>
2021-10-19 16:43:38 +0300

    private request (#11)

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -3,11 +3,25 @@ import PendingQueries from "storage/singletons/PendingQueries";
 
 const LOG_QUANTITY = false;
 
-const Loader: React.FC = () => {
+const Loader = ({ children }: { children: any }) => {
   if (LOG_QUANTITY) {
     console.log("Running queries quantity: ", PendingQueries.length);
   }
-  return PendingQueries.empty ? <></> : <p>...is loading</p>;
+
+  return PendingQueries.empty ? (
+    children
+  ) : (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        position: "absolute",
+        background: "white",
+      }}
+    >
+      <p>...is loading</p>
+    </div>
+  );
 };
 
 export default observer(Loader);
