6f25f03bb144a87d39b9ebe8f847b5d4f0939c97
k-shkrybailo <k.shkrybailo@sharksw.com>
2021-11-05 14:33:52 +0200

    toolbar with actions are search

--- src/components/common/Form/ButtonWithIcon/styles.tsx
+++ src/components/common/Form/ButtonWithIcon/styles.tsx
@@ -1,9 +1,16 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core'
 
-import { ThemeDefaultOptions } from "utils/types/themeConfig";
+import { ThemeDefaultOptions } from 'utils/types/themeConfig'
 
 const useStyles = makeStyles((theme: ThemeDefaultOptions) => ({
-  root: { padding: `${theme.spacing(1.25)}px ${theme.spacing(2.5)}px` },
-}));
+  root: {
+    padding: `${theme.spacing(1.25)}px ${theme.spacing(2.5)}px`,
+    '& svg': {
+      width: 12,
+      height: 12,
+      fill: theme.palette.secondary.main
+    }
+  }
+}))
 
-export default useStyles;
+export default useStyles
