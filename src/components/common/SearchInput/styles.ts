a0bd82e5d13d9f78410e4eccb33ddee89a5c2a4a
k-shkrybailo <67151012+k-shkrybailo@users.noreply.github.com>
2021-11-08 12:24:53 +0200

    toolbar with actions are search (#15)

---
+++ src/components/common/SearchInput/styles.ts
@@ -0,0 +1,31 @@
+import { makeStyles } from '@material-ui/core'
+
+import { ThemeDefaultOptions } from 'utils/types/themeConfig'
+
+const useStyles = makeStyles((theme: ThemeDefaultOptions) => ({
+  root: {
+    width: 400,
+    height: '100%',
+    borderRadius: 0,
+    '& svg': {
+      width: 14,
+      height: 14,
+      fill: theme.palette.secondary.icon
+    }
+  },
+  adornedStart:
+    theme.direction === 'ltr'
+      ? {
+          paddingRight: `${theme.spacing(2.5)}px`
+        }
+      : { paddingLeft: `${theme.spacing(2.5)}px` },
+  input: {
+    padding: `${theme.spacing(2.25)}px ${theme.spacing(1)}px`,
+    fontSize: '1.4rem'
+  },
+  notchedOutline: {
+    borderColor: theme.palette.primary.light
+  }
+}))
+
+export default useStyles
