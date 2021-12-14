1c96aba5424bded92b40345ffcf14e9e3f92327a
k-shkrybailo <67151012+k-shkrybailo@users.noreply.github.com>
2021-12-14 17:23:19 +0200

    2fa (#38)

---
+++ src/levels/authorization/Login/components/TwoFactorAuthenticationForm.tsx
@@ -0,0 +1,68 @@
+import { observer } from "mobx-react-lite";
+import { useTranslation } from "react-i18next";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import Button from "@material-ui/core/Button";
+
+import { twoFactorSchema } from "utils/schemas/loginSchema";
+import { TTwoFactorAuthentication } from "utils/types/authentication";
+import loginStore from "storage/singletons/Login";
+import FormInput from "components/common/Form/FormInput";
+import FormCheckbox from "components/common/Form/FormCheckbox";
+
+import { useFormStyles } from "./styles";
+
+const defaultValues = {
+  code: "",
+  trust: false,
+};
+
+const TwoFactorAuthenticationForm: React.FC = () => {
+  const { t } = useTranslation();
+  const { sendTwoFactorCode } = loginStore;
+
+  const { control, handleSubmit } = useForm<TTwoFactorAuthentication>({
+    resolver: yupResolver(twoFactorSchema(t)),
+    defaultValues,
+  });
+  const classes = useFormStyles();
+
+  const handleChange: SubmitHandler<TTwoFactorAuthentication> = payload => {
+    sendTwoFactorCode(payload);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(handleChange)}
+      className={classes.loginFormWrapper}
+    >
+      <p className={classes.twoFactorHelperMessage}>
+        {/* We are trying to log in from a new device. We have sent a code to your email for safety, please enter it below: */}
+        {t("twoFactorHelpMessage")}
+      </p>
+      <Controller
+        name="code"
+        control={control}
+        render={({ field, ...props }) => (
+          <FormInput label={t("Code")} {...field} {...props} />
+        )}
+      />
+      <Controller
+        name="trust"
+        control={control}
+        render={({ field, ...props }) => (
+          <FormCheckbox label={t("Trust this device")} {...field} {...props} />
+        )}
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        className={classes.twoFactorSubmit}
+      >
+        {t("Submit")}
+      </Button>
+    </form>
+  );
+};
+
+export default observer(TwoFactorAuthenticationForm);
