c84f194a58231915bc5d179e97d8328e73e88d91
Banul Vladyslav <v.banul@m.sharksw.com>
2021-12-23 16:35:39 +0200

    posibility to add subscription added

--- src/levels/tenant/components/SubscriptionsList/index.tsx
+++ src/levels/tenant/components/SubscriptionsList/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useMemo } from "react";
+import { FC, useEffect, useMemo, useState } from "react";
 import { observer } from "mobx-react-lite";
 import { useTranslation } from "react-i18next";
 import { TFunction } from "i18next";
@@ -7,8 +7,8 @@ import { useParams } from "react-router-dom";
 import Subscriptions from "storage/singletons/Subscriptions";
 import TableSelectedRowsStore from "storage/singletons/TableSelectedRows";
 import Table from "components/Table";
-import TableActions from "components/Table/components/TableActions";
 import { Plus, Trash } from "components/Icons";
+import AddTenantSubscription from "./components/AddTenantSubscription";
 
 const getTranslatedColumns = (t: TFunction) => [
   {
@@ -24,33 +24,15 @@ const getTranslatedColumns = (t: TFunction) => [
 const SubscriptionsList: FC = () => {
   const { t } = useTranslation();
   const params = useParams<{ tenantID: string }>();
+  const [modalToOpen, setModalToOpen] = useState("");
 
   const { getSubscriptionsData, subscriptions } = Subscriptions;
   const { setSelectedRows } = TableSelectedRowsStore;
 
-  const columns = useMemo(
-    () => [
-      ...getTranslatedColumns(t),
-      {
-        Header: t("Actions"),
-        accessor: "actions",
-        disableSortBy: true,
-        Cell: (props: any) => {
-          return (
-            <TableActions
-              edit
-              del
-              onDelete={() => {
-                // setModalToOpen("delete");
-                setSelectedRows({ [props.row.index]: true });
-              }}
-            />
-          );
-        },
-      },
-    ],
-    [t, setSelectedRows],
-  );
+  const columns = useMemo(() => [...getTranslatedColumns(t)], [
+    t,
+    setSelectedRows,
+  ]);
 
   useEffect(() => {
     getSubscriptionsData(params.tenantID);
@@ -71,11 +53,13 @@ const SubscriptionsList: FC = () => {
       title: "Add",
       icon: Plus,
       onClick: () => {
-        // setModalToOpen("add");
+        setModalToOpen("add");
       },
     },
   ];
-
+  const handleCloseModal = () => {
+    setModalToOpen("");
+  };
   return (
     <>
       <Table
@@ -85,6 +69,9 @@ const SubscriptionsList: FC = () => {
         toolbarActions={toolbarActions}
         checkbox
       />
+      {modalToOpen === "add" && (
+        <AddTenantSubscription handleCancel={handleCloseModal} />
+      )}
     </>
   );
 };
