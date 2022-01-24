5c2ed97fe7cc0d767ee61053d53030c33d075eba
VladyslavBanul <96122326+VladyslavBanul@users.noreply.github.com>
2022-01-24 13:49:52 +0200

    User profile and password editing separated (#65)

--- src/utils/schemas/profileSchema.ts
+++ src/utils/schemas/profileSchema.ts
@@ -3,6 +3,21 @@ import { string, object, ref } from "yup";
 
 export const profileEditSchema = (t: TFunction) =>
   object().shape({
+    email: string().matches(
+      /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/,
+      {
+        message: t("Incorrect email address"),
+        excludeEmptyString: true,
+      },
+    ),
+    mobile_number: string().matches(/^(\+\d{1,3}[- ]?)?\d{10}$/, {
+      message: t("Incorrect number"),
+      excludeEmptyString: true,
+    }),
+  });
+
+export const changePasswordSchema = (t: TFunction) =>
+  object().shape({
     password: string()
       .label(t("Password"))
       .when("confirmPassword", {
@@ -12,16 +27,6 @@ export const profileEditSchema = (t: TFunction) =>
       }),
     confirmPassword: string()
       .label(t("Confirm password"))
-      .oneOf([ref("password"), null], "Passwords must match"),
-    email: string().matches(
-      /^([a-zA-Z0-9_\-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/,
-      {
-        message: "Incorrect email address",
-        excludeEmptyString: true,
-      },
-    ),
-    mobile_number: string().matches(/^(\+\d{1,3}[- ]?)?\d{10}$/, {
-      message: "Incorrect number",
-      excludeEmptyString: true,
-    }),
+      .oneOf([ref("password"), null], t("Passwords must match")),
+    old_password: string().required().label(t("Old password")),
   });
---
+++ src/utils/types/user/index.ts
@@ -0,0 +1,11 @@
+export type changePasswordPayload = {
+  old_password: string;
+  password: string;
+};
+
+export type changeAccountInfoPayload = {
+  first_name: string;
+  last_name: string;
+  email: string;
+  mobile_number: string;
+};
