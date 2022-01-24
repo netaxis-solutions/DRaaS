b886491955ed3ffa1d8574d657134394084be53c
Sergey Koliadintsev <s.koliadintsev@m.sharksw.com>
2022-01-24 13:52:53 +0200

    remove t

--- src/components/TopBar/components/IconsBar/IconButtons/components/AccountModal/index.tsx
+++ src/components/TopBar/components/IconsBar/IconButtons/components/AccountModal/index.tsx
@@ -1,5 +1,4 @@
 import { observer } from "mobx-react-lite";
-import { useTranslation } from "react-i18next";
 
 import Modal from "components/Modal";
 import AccountInfo from "./AccountInfo";
@@ -7,8 +6,6 @@ import AccountInfo from "./AccountInfo";
 const EditUserProfile: React.FC<{ handleCancel: () => void }> = ({
   handleCancel,
 }) => {
-  const { t } = useTranslation();
-
   return (
     <Modal title={""} handleCancel={handleCancel} isBackIconHidden={true}>
       <AccountInfo handleCancel={handleCancel} />
