40fb1d99ed577f8cfc487f4661fc4cad48fb2b31
Banul Vladyslav <v.banul@m.sharksw.com>
2022-01-27 09:32:56 +0200

    suggestions

--- src/levels/admin/components/CurrentVersionInfo/index.tsx
+++ src/levels/admin/components/CurrentVersionInfo/index.tsx
@@ -1,14 +1,22 @@
+import { useTranslation } from "react-i18next";
 import preval from "preval.macro";
 import packageJson from "../../../../../package.json";
 
-const buildTimestamp = preval`module.exports = new Date().getTime();`;
+const buildTimestamp = new Date(preval`module.exports = new Date().getTime();`);
 
 const BuildInfo: React.FC = () => {
+  const { t } = useTranslation();
   return (
     <>
-      <div>Current version: {packageJson.version}</div>
-      <div>Last build: {`${new Date(buildTimestamp)}`}</div>
-      <div>Last commit: {process.env.REACT_APP_GIT_SHA}</div>
+      <div>
+        {t("Current version")}: {packageJson.version}
+      </div>
+      <div>
+        {t("Last build")}: {`${buildTimestamp}`}
+      </div>
+      <div>
+        {t("Last commit")}: {process.env.REACT_APP_GIT_SHA}
+      </div>
     </>
   );
 };
