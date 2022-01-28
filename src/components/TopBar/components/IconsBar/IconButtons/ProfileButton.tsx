2398c654cfb4ba353505456b807f89dab0ad2fd8
VladyslavBanul <96122326+VladyslavBanul@users.noreply.github.com>
2022-01-28 13:01:54 +0200

    Version info (#72)

--- src/components/TopBar/components/IconsBar/IconButtons/ProfileButton.tsx
+++ src/components/TopBar/components/IconsBar/IconButtons/ProfileButton.tsx
@@ -3,6 +3,7 @@ import AppMenu from "@material-ui/core/Menu";
 import { useTranslation } from "react-i18next";
 
 import LoginStore from "storage/singletons/Login";
+import RoutingConfig from "storage/singletons/RoutingConfig";
 
 import { Login } from "components/Icons";
 import AccountModal from "./components/AccountModal";
@@ -16,6 +17,7 @@ const ProfileButton: React.FC = () => {
   const [anchorEl, setAnchorEl] = useState(null);
   const [modalToOpen, setModal] = useState("");
 
+  const { history, loggedInUserLevel } = RoutingConfig;
   const { user, logout } = LoginStore;
 
   const handleMenu = (e: any) => {
@@ -57,6 +59,14 @@ const ProfileButton: React.FC = () => {
         >
           {t("Change password")}
         </div>
+        {loggedInUserLevel === "admin" && (
+          <div
+            className={classes.account}
+            onClick={() => history.push("/info")}
+          >
+            {t("Version info")}
+          </div>
+        )}
         <div className={classes.logOut} onClick={() => logout()}>
           {t("Logout")}
         </div>
