1681f0e787f20f7ef0e926c8e0c916c3cbc96ef0
SergeyKoliadintsev <94185679+SergeyKoliadintsev@users.noreply.github.com>
2022-02-04 17:34:59 +0200

    fix bug (#76)

--- src/utils/schemas/license.ts
+++ src/utils/schemas/license.ts
@@ -1,8 +1,15 @@
+import { TFunction } from "i18next";
 import { string, object } from "yup";
 
 export const addLicenseSchema = () => object().shape({});
 
-export const editLicenseSchema = () =>
+export const editLicenseSchema = (t: TFunction) =>
   object().shape({
-    assigned: string().required(),
+    assigned: string()
+      .matches(/^\d+(?:\.\d+)?$/, {
+        message: t(
+          "Incorrect assigned number. Number must not be negative or greater than inUse ",
+        ),
+      })
+      .required(),
   });
