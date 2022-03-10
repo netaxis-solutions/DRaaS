caaccf63c28d1dee48f9002992fe89031d8e7e0b
MortyQ <yasenev1998@gmail.com>
2022-03-10 09:40:57 +0200

    update types

--- src/utils/types/themeConfig/index.ts
+++ src/utils/types/themeConfig/index.ts
@@ -17,12 +17,22 @@ export type PaletteDefaultOptionsType = PaletteOptions & {
   sidebar: ColorType;
 };
 
+export type TThemeBody = {
+  background: string;
+  fontFamily: string;
+  button: { [key: string]: string | { [key: string]: string } };
+  table: { [key: string]: string | { [key: string]: string } };
+  tabs: { [key: string]: string | { [key: string]: string } };
+  card: { [key: string]: string | { [key: string]: string } };
+  stepper: { [key: string]: string | { [key: string]: string } };
+};
+
 export type ThemeDefaultOptions = Theme & {
   palette: PaletteDefaultOptionsType;
   spacing: SpacingOptions | ((num: number) => number);
-  body: any;
-  top_bar: any;
-  side_bar: any;
+  body: TThemeBody;
+  top_bar: { [key: string]: string };
+  side_bar: { [key: string]: string };
 };
 
 export type DeepCloneThemeType = (
