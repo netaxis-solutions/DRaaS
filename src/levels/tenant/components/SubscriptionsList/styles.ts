bde76f77ca9b71adb1059abb54bc43913fb50011
MortyQ <yasenev1998@gmail.com>
2022-04-01 13:52:00 +0300

    remove empty table in subscription

---
+++ src/levels/tenant/components/SubscriptionsList/styles.ts
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core";
+
+export default makeStyles(() => ({
+  emptyTableWrapper: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+}));
