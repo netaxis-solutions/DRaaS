a625a4ecc8af1e06a67dcde4fd683128c6b469c1
VladyslavBanul <v.banul@m.sharksw.com>
2022-05-17 09:14:52 +0300

    hotfix

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import App from "components/App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Suspense fallback={<></>}>
-      <App />
-    </Suspense>
+    <App />
   </React.StrictMode>,
   document.getElementById("root"),
 );
