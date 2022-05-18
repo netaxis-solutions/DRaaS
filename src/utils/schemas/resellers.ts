eddb3567288fcfc7e2f3b5fa561822342c4145de
VladyslavBanul <v.banul@m.sharksw.com>
2022-05-18 11:46:19 +0300

    fxed

--- src/utils/schemas/resellers.ts
+++ src/utils/schemas/resellers.ts
@@ -1,4 +1,5 @@
 import { TFunction } from "i18next";
+import RoutingConfig from "storage/singletons/RoutingConfig";
 import { string, object } from "yup";
 
 export const addResellerSchema = (t: TFunction) =>
@@ -12,7 +13,11 @@ export const addResellerSchema = (t: TFunction) =>
     billingId: string(),
     distributor: object().shape({
       label: string(),
-      value: string().required(t("Please choose a distributor")),
+      value: string().when("name", {
+        is: (_: string) => RoutingConfig.loggedInUserLevel === "system",
+        then: string().required(t("Please choose a distributor")),
+        otherwise: string(),
+      }),
     }),
     markup: string().matches(
       /^([0-9]\.[0-9]{1}|[0-9]\.[0-9]{2}|\.[0-9]{2}|[1-9][0-9]\.[0-9]{1}|[1-9][0-9]\.[0-9]{2}|[0-9][0-9]|[1-9][0-9]\.[0-9]{2})$|^([0-9]|[0-9][0-9]|[0-99])$|^100$/,
