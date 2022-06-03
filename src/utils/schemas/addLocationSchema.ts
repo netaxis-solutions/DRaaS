1fffdfbac875bbda545f4fd2f67ded328e1e32b9
SergeyKoliadintsev <yasenev1998@gmail.com>
2022-06-03 09:51:12 +0300

    Add validation to Profile page. User name, Postal code, numbers , postal cod and city

--- src/utils/schemas/addLocationSchema.ts
+++ src/utils/schemas/addLocationSchema.ts
@@ -7,9 +7,18 @@ export const addLocationSchema = (t: TFunction) =>
       label: string(),
       value: string().required(t("This field is mandatory")),
     }),
-    street: string().required(t("This field is mandatory")),
-    number: string().required(t("This field is mandatory")),
-    postbox: string(),
+    street: string().matches(/^[aA-zZ0-9\s]+$/, {
+      message: t("Use only letters and digits"),
+      excludeEmptyString: true,
+    }).required(t("This field is mandatory")),
+    number: string().matches(/^[aA-zZ0-9\s]+$/, {
+      message: t("Use only letters and digits"),
+      excludeEmptyString: true,
+    }).required(t("This field is mandatory")),
+    postbox: string().matches(/^[aA-zZ0-9\s]+$/, {
+      message: t("Use only letters and digits"),
+      excludeEmptyString: true,
+    }),
     agreement: boolean()
       .required()
       .oneOf(
