bfbb9756a6b446e6889efba9548a55aa880de2c5
VladyslavBanul <v.banul@m.sharksw.com>
2022-07-20 10:00:26 +0300

    error message changed

--- src/utils/schemas/addLocationSchema.ts
+++ src/utils/schemas/addLocationSchema.ts
@@ -15,14 +15,14 @@ export const addLocationSchema = (t: TFunction) =>
       .required(t("This field is mandatory")),
     number: string()
       .matches(/^[0-9\s]+$/, {
-        message: t("Use only letters and digits"),
+        message: t("Use only digits"),
         excludeEmptyString: true,
       })
       .max(30)
       .required(t("This field is mandatory")),
     postbox: string()
       .matches(/^[aA-zZ0-9\s]+$/, {
-        message: t("Use only digits"),
+        message: t("Use only letters and digits"),
         excludeEmptyString: true,
       })
       .max(100),
